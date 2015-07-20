fd82c4f37008680b5cbfc3ab06e7034afc80cb41
TheNotary <no@mail.plz>
2015-07-20 15:41:49 -0500

    made MyUI object nested

--- public/unrelated_tools/my_ui.js
+++ public/unrelated_tools/my_ui.js
@@ -1,5 +1,11 @@
 var MyUI = function(){
-  
+  this.TabSwitcher = new TabSwitcher();
+  this.SourceCodePresenter = new SourceCodePresenter();
+};
+
+// These functions are how we can switch what content is displayed based 
+// on which tab is activated by the user
+var TabSwitcher = function(){
   this.displayNotes = function(e) {
     this.hideAllNotes();
     var notes = e.getAttribute("data-name");
@@ -16,6 +22,12 @@ var MyUI = function(){
     this.removeActiveness();
     $("li."+targ).addClass("active");
   };
+};
+
+// These functions relate to rendering source code in a .box
+// on mouse over of links with classes that match up with data-trigger 
+// values placed elswhere which contain source code to present
+var SourceCodePresenter = function(){ 
   // Pass in the selector of the content you'd like presented somewhere
   // and the selector for the html element you'd like updated with content
   this.linkHoverActionToPresentationOfSourceCode = function(contentSelector, renderPaneSelector) {
@@ -35,24 +47,18 @@ var MyUI = function(){
           box.hide();
         });
       });
-      
     });
-
   };
   
 };
 
-window.MyUI = new MyUI();
 
-//MyUI.prototype.SourceCodePresenter = {
-  
-
-//};
+window.myUI = new MyUI();
 
 
 
 $().ready(function() {
-  MyUI.linkHoverActionToPresentationOfSourceCode('.box', '.source-view');
+  myUI.SourceCodePresenter.linkHoverActionToPresentationOfSourceCode('.box', '.source-view');
 });
 
 
